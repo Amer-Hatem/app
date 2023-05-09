95f6ebdb3e9dc4c60d8da3a048d78592995e95fb
Amer-Hatem <133026858+Amer-Hatem@users.noreply.github.com>
2023-05-09 18:09:44 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react'
+import About from './Components/About/About.jsx'
+import Home from './Components/Home/Home.jsx'
+
+export default class App extends Component {
+  render() {
+    return (
+   <div>
+    <About />
+    <Home />
+    
+   </div>
+
+    )
+  }
+}
